4bd7bfa68f185eb21704bf667215b7dbf002ff56
sevenpoh <vinhobistroo@gmail.com>
2025-04-09 04:59:52 -0300

    Delete bot.py

--- bot.py
+++
@@ -1,9 +0,0 @@
-# Exemplo simples de bot
-from flask import Flask
-app = Flask(__name__)
-@app.route('/')
-def hello():
-    return 'Bot ativo!'
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
